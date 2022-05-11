b19094e77a16181c7068b9a22c143da29efa42fd
Hagyeong Kim <hagyeong.kim@digipen.edu>
2022-05-11 02:31:49 -0700

    youtube

--- 22Summer/Assets/test.cs
+++ 22Summer/Assets/test.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)) //0 is left mouse button
+        {
+            rb.velocity = Vector3.up * 3; //Vector2.up * 3 means (0,3)
+        }
     }
 }
