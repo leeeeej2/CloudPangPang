39f6cf38d308370e412b8d42d46d7a60810e8e27
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-06-02 00:26:48 -0700

    no message

--- Assets/Accelerometer.cs
+++ Assets/Accelerometer.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Accelerometer : MonoBehaviour
 {
-    
+    public bool isFlat = true;
+    private Rigidbody rigid;
+    private void Start() {
+       rigid = GetComponent<Rigidbody>();
+   }
+
+   private void Update() {
+       Vector3 tilt = Input.acceleration;
+
+        if(isFlat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+       rigid.AddForce(Input.acceleration);
+       Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+   }
 }
