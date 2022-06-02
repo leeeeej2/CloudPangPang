9d3d9e6c7aacdb1ff500e3f4fa89d3b10531ca04
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-06-02 02:30:04 -0700

    Add accelerometer script for aircraft to move left and right with the accelerometer sensor

--- Assets/Accelerometer.cs
+++ Assets/Accelerometer.cs
@@ -6,19 +6,30 @@ public class Accelerometer : MonoBehaviour
 {
     public bool isFlat = true;
     private Rigidbody rigid;
+    float dirX;
+    float moveSpeed = 20f;
+
     private void Start() {
        rigid = GetComponent<Rigidbody>();
    }
 
    private void Update() {
-       Vector3 tilt = Input.acceleration;
+      
+      dirX = Input.acceleration.x * moveSpeed;
+      transform.position = new Vector3(Mathf.Clamp(transform.position.x, -16f, 16f), transform.position.y, transform.position.z);
+       //Vector3 tilt = Input.acceleration;
 
-        if(isFlat)
-        {
-            tilt = Quaternion.Euler(90, 0, 0) * tilt;
-        }
+        //if(isFlat)
+        //{
+        //    tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        //}
 
-       rigid.AddForce(Input.acceleration);
-       Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+       //rigid.AddForce(Input.acceleration);
+       //Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+       
+   }
+
+   private void FixedUpdate() {
+       rigid.velocity = new Vector3(dirX, 0, 0);
    }
 }
