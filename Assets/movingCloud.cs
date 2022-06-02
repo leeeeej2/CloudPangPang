0e25a2fdd3c1c959ff48f818587ad3b683a938b3
Hagyeong Kim <hagyeong.kim@digipen.edu>
2022-06-02 03:29:37 -0700

    change cloud
    change cloud range & scale

--- Assets/movingCloud.cs
+++ Assets/movingCloud.cs
@@ -6,6 +6,8 @@ public class movingCloud : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
+    float randomSpeed;
+    bool once = true;
     void Start()
     {
         
@@ -14,6 +16,12 @@ public class movingCloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        randomSpeed = Random.Range(0, 2);
+        if(once)
+        {
+            speed += randomSpeed;
+            once = false;
+        }
         transform.position -= Vector3.forward * Time.deltaTime * speed;
     }
 }
