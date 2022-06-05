674b273f823973025ec079599137cadd17961487
Hagyeong Kim <hagyeong.kim@digipen.edu>
2022-06-04 21:59:59 -0700

    change cloud

--- Assets/makeCloud.cs
+++ Assets/makeCloud.cs
@@ -7,12 +7,22 @@ public class makeCloud : MonoBehaviour
     // Start is called before the first frame update
     public GameObject cloud;
     public GameObject cloud2;
-    public GameObject cloud3;
     public GameObject mainObject;
+
+    public float xMax = -15f;
+    public float xMin = -50f;
+
+    public float yMax = 0;
+    public float yMin = 25f;
+
+    public float zMax = 25f;
+    public float zMin = 30f;
 
     public float timer = 0;
     public float timeDiff = 1.5f;
     public float destoryTime = 10.0f;
+
+    GameObject newCloud;
 
     void Start()
     {
@@ -30,27 +40,25 @@ public class makeCloud : MonoBehaviour
             //x -10 ~ 10
             //y -15 ~ 15
 
-            GameObject newCloud;
-
             whatType = Random.Range(0, 2);
+
+            Debug.Log(whatType);
 
             if(whatType == 0)
             {
                 newCloud = Instantiate(cloud);
             }
-            else if(whatType == 1)
+            
+            if(whatType == 1)
             {
                 newCloud = Instantiate(cloud2);
-            }
-            else
-            {
-                newCloud = Instantiate(cloud3);
             }
 
             Vector3 mainPosition = mainObject.transform.position;
             
-            newCloud.transform.position = new Vector3(Random.Range(-40f, -15f), Random.Range(0, 25f), Random.Range(25f, 30f));
-            newCloud.transform.localScale = new Vector3(Random.Range(0.5f, 1), Random.Range(0.5f, 1), Random.Range(0.5f, 1));
+            newCloud.transform.position = new Vector3(Random.Range(xMin , xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
+            float cloudSize = Random.Range(0.2f, 0.5f);
+            newCloud.transform.localScale = new Vector3(cloudSize, cloudSize, cloudSize);
             
             timer = 0;
 
