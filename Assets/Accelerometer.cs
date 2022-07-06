8b65c4fb926ab8cbffad39c131c0b36658152a18
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-07-06 00:38:04 -0700

    Revert "add control"
    This reverts commit e16288159f2034a45370629af5b6bb17dff68cf7.

--- Assets/Accelerometer.cs
+++ Assets/Accelerometer.cs
@@ -9,57 +9,18 @@ public class Accelerometer : MonoBehaviour
     float dirX;
     float moveSpeed = 25f;
 
-    /////////for pc control
-    float moveX = 0f;
-    float moveY = 0f;
-    float moveZ = 0f;
-    /////////
-    
     private void Start() {
        rigid = GetComponent<Rigidbody>();
-
-        moveX = transform.position.x;
-        moveY = transform.position.y;
-        moveZ = transform.position.z;
-    }
+   }
 
    private void Update() {
       
-        dirX = Input.acceleration.x * moveSpeed;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -16f, 16f), transform.position.y, transform.position.z);
+      dirX = Input.acceleration.x * moveSpeed;
+      transform.position = new Vector3(Mathf.Clamp(transform.position.x, -16f, 16f), transform.position.y, transform.position.z);
 
-        /////////for pc control
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("left");
-            moveX--;
-            transform.position = new Vector3(moveX, moveY, moveZ);
-        }
+   }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-             Debug.Log("right");
-             moveX++;
-            transform.position = new Vector3(moveX, moveY, moveZ);
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            Debug.Log("up");
-            moveY++;
-            transform.position = new Vector3(moveX, moveY, moveZ);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log("down");
-            moveY--;
-            transform.position = new Vector3(moveX, moveY, moveZ);
-        }
-        /////////
-    }
-
-    private void FixedUpdate() {
+   private void FixedUpdate() {
        rigid.velocity = new Vector3(dirX, 0, 0);
    }
 }
