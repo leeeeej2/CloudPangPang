3ffe7c4fa8609fa6598b63de0081db4ea22f97cf
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-07-07 15:48:43 -0700

    select random color when clouds are spawned in spawner.

--- Assets/makeCloud.cs
+++ Assets/makeCloud.cs
@@ -24,9 +24,29 @@ public class makeCloud : MonoBehaviour
 
     GameObject newCloud;
 
+    Color white;
+    Color yellow;
+    Color pink;
+    Color blue;
+
+    public Color[] myColors;
+    public Color pickColor;
+    //public Renderer myRenderer;
+
+    private void Awake() {
+        white = new Color(1f, 1f, 1f, 1f);
+        yellow = new Color(1f, 0.835f, 0.454f, 1f);
+        pink = new Color(1f, 0.686f, 1f, 1f);
+        blue = new Color(0.431f, 0.666f, 1f, 1f);
+    }
+
     void Start()
     {
-        
+        myColors[0] = white;
+        myColors[1] = yellow;
+        myColors[2] = pink;
+        myColors[3] = blue;
+        //myRenderer = gameObject.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -52,12 +72,16 @@ public class makeCloud : MonoBehaviour
                 newCloud = Instantiate(cloud2);
             }
 
+            pickColor = myColors[Random.Range(0, myColors.Length)];
+            newCloud.GetComponent<MeshRenderer>().material.color = pickColor;
+
             Vector3 mainPosition = mainObject.transform.position;
             newCloud.transform.position = new Vector3(Random.Range(xMin , xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
             //newCloud.transform.position = new Vector3(-30f, 16.3f, 6.5f);
             float cloudSize = Random.Range(0.2f, 0.5f);
             newCloud.transform.localScale = new Vector3(cloudSize, cloudSize, cloudSize);
-            
+
+
             timer = 0;
 
             Destroy(newCloud, destoryTime);
