0ab8d28cd3698ec1283eb63e55141fbd62ecdecb
Hagyeong Kim <hagyeong.kim@digipen.edu>
2022-07-08 16:27:33 -0700

    new cloud

--- Assets/makeCloud.cs
+++ Assets/makeCloud.cs
@@ -8,6 +8,7 @@ public class makeCloud : MonoBehaviour
     public GameObject cloud;
     public GameObject cloud2;
     public GameObject mainObject;
+    public GameObject mainObject2;
 
     public float xMax = -15f;
     public float xMin = -50f;
@@ -21,7 +22,15 @@ public class makeCloud : MonoBehaviour
     public float timer = 0;
     public float timeDiff = 1.5f;
     public float destoryTime = 10.0f;
+    public float destoryOffset = 5f;
 
+    float xCloudRangeMax;
+    float xCloudRangeMin;
+
+    float yCloudRangeMax;
+    float yCloudRangeMin;
+
+    float destroyLocation;
     GameObject newCloud;
 
     public static Color white;
@@ -32,6 +41,7 @@ public class makeCloud : MonoBehaviour
     public Color[] myColors;
     public Color pickColor;
     //public Renderer myRenderer;
+
 
     private void Awake() {
         white = new Color(1f, 1f, 1f, 1f);
@@ -46,6 +56,34 @@ public class makeCloud : MonoBehaviour
         myColors[1] = yellow;
         myColors[2] = pink;
         myColors[3] = blue;
+
+        float xRange = 2 * Camera.main.orthographicSize;//2 * (Screen.height / 10) / 2;
+        float yRange = xRange * Camera.main.aspect;
+
+        float xOffset = xOffset = xRange + transform.localScale.x;
+        float yOffset = yOffset = (yRange / 2) - (transform.localScale.y);
+
+        Vector3 ScreenCenter = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+
+        xCloudRangeMax = mainObject.transform.position.x + xOffset - 1;
+        xCloudRangeMin = mainObject.transform.position.x - xOffset + 1;
+
+        yCloudRangeMax = mainObject.transform.position.y + yOffset - 1;
+        yCloudRangeMin = mainObject.transform.position.y - yOffset + 1;
+
+        destroyLocation = mainObject2.transform.position.z;
+
+        //Debug.Log("destroy location");
+        //Debug.Log(destroyLocation);
+
+        //Debug.Log("x max");
+        //Debug.Log(xCloudRangeMax);
+        //Debug.Log("x min");
+        //Debug.Log(xCloudRangeMin);
+        //Debug.Log("y max");
+        //Debug.Log(yCloudRangeMax);
+        //Debug.Log("y min");
+        //Debug.Log(yCloudRangeMin);
         //myRenderer = gameObject.GetComponent<Renderer>();
     }
 
@@ -59,7 +97,7 @@ public class makeCloud : MonoBehaviour
         {
             //x -10 ~ 10
             //y -15 ~ 15
-
+        
             whatType = Random.Range(0, 2);
 
             if(whatType == 0)
@@ -76,15 +114,19 @@ public class makeCloud : MonoBehaviour
             newCloud.GetComponent<MeshRenderer>().material.color = pickColor;
 
             Vector3 mainPosition = mainObject.transform.position;
-            newCloud.transform.position = new Vector3(Random.Range(xMin , xMax), Random.Range(yMin, yMax), Random.Range(zMin, zMax));
+            newCloud.transform.position = new Vector3(Random.Range(xCloudRangeMin , xCloudRangeMax), Random.Range(yCloudRangeMin, yCloudRangeMax), Random.Range(zMin, zMax));
             //newCloud.transform.position = new Vector3(-30f, 16.3f, 6.5f);
             float cloudSize = Random.Range(0.2f, 0.5f);
             newCloud.transform.localScale = new Vector3(cloudSize, cloudSize, cloudSize);
 
-
             timer = 0;
 
-            Destroy(newCloud, destoryTime);
+            //if(newCloud.transform.position.z == destroyLocation)
+            //{
+                //Debug.Log("destroy cloud!!");
+                //Debug.Log(newCloud.transform.position.z);
+                Destroy(newCloud, destoryTime);
+            //}
         }
     }
 }
