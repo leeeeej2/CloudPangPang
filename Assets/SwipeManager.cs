7ea18d7de33fdc41ac692aeef6a8207b7be1cba6
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-07-11 15:18:51 -0700

    import new heart icon and change the UI

--- Assets/SwipeManager.cs
+++ Assets/SwipeManager.cs
@@ -8,12 +8,6 @@ public class SwipeManager : MonoBehaviour
     private Vector3 lastPosition;
     
     private float dragDistance = 0.25f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
 
     // Update is called once per frame
     void Update()
